e6c2cd557aa13ae4a09eed4af3cc0f8140f8a249
Alex Ghit <76708587+AlexGhit@users.noreply.github.com>
2022-05-18 16:08:01 +0200

    Create terminal_lines.tcl

---
+++ terminal_lines.tcl
@@ -0,0 +1,5 @@
+// you can write (pwd) on the terminal to know what is the place that you are in right now 
+// you can write (mkdir) on the terminal to make a new folder or a place on the place that you are in right now
+// you can write (ls) to show all the folders on that folder 
+// you can close the programe by using (killall) the the name of the thing that you want to close after it
+// you can go to folders or places on your computer from (cd) code and write your place name after it to go to it 
